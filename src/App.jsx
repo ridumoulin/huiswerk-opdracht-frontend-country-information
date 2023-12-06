5fa8944aa5cf4e918bfa4a8b41782a70d7a5844d
RiDumoulin <riannedumoulin@gmail.com>
2023-12-06 13:01:53 +0100

    Opzetten opdracht

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css';
+import axios from 'axios';
+
+async function fetchCountries() {
+    try {
+        const result = await axios.get('https://restcountries.com/v3.1/all');
+        console.log(result);
+    }   catch (e) {
+        console.error(e);
+    }
+}
 
 function App() {
 
     return (
         <>
-            Maak je applicatie hier!
+            <button type="button" onClick={fetchCountries}>
+                Haal landen op!
+            </button>
         </>
     )
 }
